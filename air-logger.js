76a61c08e40177849358d55778fa38c7b7d52722
baali <baali@muse-amuse.in>
2015-04-12 18:10:06 +0530

    Added function to log temprature data.

--- air-logger.js
+++ air-logger.js
@@ -4,27 +4,35 @@
 var m = require('mraa'); //require mraa
 console.log('MRAA Version: ' + m.getVersion()); //write the mraa version to the console
 var sleep = require("sleep");
-var myLed = new m.Gpio(13); //LED hooked up to digital pin 13 (or built in pin on Galileo Gen1 & Gen2)
+var myLed = new m.Gpio(8); //LED hooked up to digital pin 13 (or built in pin on Galileo Gen1 & Gen2)
 myLed.dir(m.DIR_OUT); //set the gpio direction to output
 var ledState = true; //Boolean to hold the state of Led
-periodicActivity(); //call the periodicActivity function
 var analogPin0 = new m.Aio(0);
 var analogValue = 0.0;
 
-function periodicActivity()
-{
-  myLed.write(1); //if ledState is true then write a '1' (high) otherwise write a '0' (low)
-  setInterval(function() {
-    analogValue = analogPin0.readFloat();
-    console.log("Voltage measured:");
-    console.log(analogValue * (5.0/1024));
-    console.log("Dust Density:");
-    console.log(0.17 * (analogValue * (5.0/1024)) - 0.1);
-  }.bind(this), 280);
+// papaLoop(); //call the periodicActivity function
+tempLoop();
 
-  setInterval(function() {
-    myLed.write(0); 
-  }.bind(this), 40);
-  
-  setTimeout(periodicActivity,1000); //call the indicated function after 1 second (1000 milliseconds)
+function papaLoop()
+{
+  myLed.write(1);
+  sleep.usleep(280);
+  analogValue = analogPin0.read();
+  sleep.usleep(40);
+  myLed.write(0);   
+
+  console.log("Raw Signal Value (0-1023): ", analogValue);
+  console.log("Voltage measured:", analogValue * (3.3/1024));
+  console.log("Dust Density:", 0.17 * (analogValue * (3.3/1024)) - 0.1);
+  sleep.usleep(9680);
+  setTimeout(papaLoop,1000); //call the indicated function after 1 second (1000 milliseconds)
+}
+
+function tempLoop()
+{
+  analogValue = analogPin0.read();
+  console.log("Raw Signal Value: ", analogValue);
+  console.log("Temprature(C) :", analogValue *  0.48828125);
+  sleep.usleep(1000);
+  setTimeout(tempLoop,1000); //call the indicated function after 1 second (1000 milliseconds)
 }
